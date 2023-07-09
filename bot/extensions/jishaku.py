34112979653ecda7f16696b9fd261411004030d9
kyomi <me@kyomi.dev>
2023-07-09 18:05:59 -0300

    feat(ext/jishaku): Add all the features from Jishaku

--- bot/extensions/jishaku.py
+++ bot/extensions/jishaku.py
@@ -15,14 +15,28 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
+from jishaku.features.filesystem import FilesystemFeature
+from jishaku.features.guild import GuildFeature
 from jishaku.features.invocation import InvocationFeature
 from jishaku.features.management import ManagementFeature
+from jishaku.features.python import PythonFeature
 from jishaku.features.root_command import RootCommand
+from jishaku.features.shell import ShellFeature
+from jishaku.features.voice import VoiceFeature
 
 from bot.core import IBot
 
 
-class Jishaku(RootCommand, ManagementFeature, InvocationFeature):
+class Jishaku(
+    VoiceFeature,
+    GuildFeature,
+    FilesystemFeature,
+    InvocationFeature,
+    ShellFeature,
+    PythonFeature,
+    ManagementFeature,
+    RootCommand,
+):
     """Custom Jishaku cog. This is used to manage the bot."""
 
     emote = "<:spfc:1116855346338742363>"
